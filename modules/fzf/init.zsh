ccc43d1ab7ba826879a2f55472189c2969f1f9fd
Gabriel Adomnicai <gabesoft@gmail.com>
2018-12-25 16:21:00 -0800

    Ensure fzf works on nix

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -1,2 +1,6 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='ag  --hidden -l -g ""'
+
+if [ -n "${commands[fzf-share]}" ]; then
+    source "$(fzf-share)/key-bindings.zsh"
+fi
